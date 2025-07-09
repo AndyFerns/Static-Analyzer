34c2a4cd62b1e909518fe6978f5aecd78d037784
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-10 02:48:24 +0530

    separated export logic

--- main.c
+++ main.c
@@ -3,55 +3,10 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include "scanner.h"
+#include "export.h"
 
 #define GREEN "\033[1;32m"
 #define RESET "\033[0m"
-
-void save_as_json(Warning* warnings, int count) {
-    mkdir("outputs");
-    FILE* f = fopen("outputs/summary.json", "w");
-    if (!f) {
-        printf("[ERROR] Could not write JSON\n");
-        return;
-    }
-
-    fprintf(f, "[\n");
-    for (int i = 0; i < count; i++) {
-        fprintf(f,
-            "  {\n"
-            "    \"file\": \"%s\",\n"
-            "    \"line\": %d,\n"
-            "    \"pattern\": \"%s\",\n"
-            "    \"message\": \"%s\"\n"
-            "  }%s\n",
-            warnings[i].file, warnings[i].line, warnings[i].pattern, warnings[i].message,
-            (i < count - 1 ? "," : "")
-        );
-    }
-    fprintf(f, "]\n");
-    fclose(f);
-    printf(GREEN "[INFO]" RESET " Saved output to outputs/summary.json\n");
-}
-
-void save_as_csv(Warning* warnings, int count) {
-    mkdir("outputs");
-    FILE* f = fopen("outputs/summary.csv", "w");
-    if (!f) {
-        printf("[ERROR] Could not write CSV\n");
-        return;
-    }
-
-    fprintf(f, "file,line,pattern,message\n");
-    for (int i = 0; i < count; i++) {
-        fprintf(f, "\"%s\",%d,\"%s\",\"%s\"\n",
-                warnings[i].file,
-                warnings[i].line,
-                warnings[i].pattern,
-                warnings[i].message);
-    }
-    fclose(f);
-    printf(GREEN "[INFO]" RESET " Saved output to outputs/summary.csv\n");
-}
 
 int main(int argc, char* argv[]) {
     printf(GREEN "Simple Static Security Scanner for .c files\n" RESET);
