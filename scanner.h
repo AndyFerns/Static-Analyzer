c470b15cbb62263b695a3e69f129bbb238f819ed
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-09 23:35:45 +0530

    added warning flag struct

--- scanner.h
+++ scanner.h
@@ -1,6 +1,14 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+//for warning flag
+typedef struct Warning {
+    char file[512];
+    int line;
+    char pattern[32];
+    char message[256];
+} Warning;
+
 void scan_file(const char* filepath);
 void scan_directory_recursive(const char* path);
 
