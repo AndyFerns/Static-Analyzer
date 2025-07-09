336888177ad86aaf22677a5c35f18ceea2479616
AndyFerns <write2andrew.fernandes@gmail.com>
2025-07-09 23:10:07 +0530

    added printf() logs

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include "scanner.h"
+
+int main(int argc, char* argv[]) {
+    printf("Simple Static Security Scanner for .c files\n");
+
+    if (argc < 2) {
+        printf("Usage: %s <file_or_directory_to_scan>\n", argv[0]);
+        return 1;
+    }
+
+    struct stat path_stat;
+    if (stat(argv[1], &path_stat) != 0) {
+        perror("stat() error");
+        return 1;
+    }
+
+    if (path_stat.st_mode & S_IFDIR) {
+        scan_directory(argv[1]);
+    } else if (path_stat.st_mode & S_IFREG) {
+        scan_file(argv[1]);
+    } else {
+        printf("Unknown file type: %s\n", argv[1]);
+    }
+
+    return 0;
+}
